0e2aca0558ad091aeebc71a4ccbb338bd60610d6
David Fallah <davidfallah@atp-group.com>
2018-06-15 13:39:37 +0100

    Minimal code needed to reproduce issue

---
+++ app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <button @click="count++">Click me</button>
+    <Foo :message="message"/>
+  </div>
+</template>
+
+<script>
+import Foo from 'lib';
+
+export default {
+  name: 'app',
+  components: {
+    Foo
+  },
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  computed: {
+    message: function () {
+      return `Pressed: ${this.count} times`
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lib/src/Foo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script lang="js">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    message: String
+  }
+})
+</script>
